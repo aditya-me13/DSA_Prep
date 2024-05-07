9cb288aa9fce51292dd333afe1fea9a59ea97e6f
Aditya Mehta <aditya.mehta@iitgn.ac.in>
2024-05-08 00:27:41 +0530

    Create Remove_nth_Node.cpp

---
+++ LinkedList1/Remove_nth_Node.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *back = head;
+        ListNode *front = head;
+
+        for(int i = 0; i < n; i ++){
+            front = front->next;
+        }
+        
+        if(front == nullptr){
+            head = head->next;
+            return head;
+        }
+
+        while(front->next != nullptr){
+            front = front->next;
+            back = back->next;
+        }
+
+        back->next = back->next->next;
+        return head;
+        
+    }
+};
