a2d401dffb983161d463eed249535fb5817c5d6a
Aditya Mehta <aditya.mehta@iitgn.ac.in>
2024-05-15 23:49:56 +0530

    Create Unique_Paths.cpp

---
+++ Array3/Unique_Paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int no_paths(vector<vector<int>> &dp, int m, int n){
+        if(m < 0 || n < 0){
+            return 0;
+        }
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+        dp[m][n] = no_paths(dp, m-1, n) + no_paths(dp, m, n-1);
+        return dp[m][n];
+    }
+
+    int uniquePaths(int m, int n) {
+        if(m == 1 && n == 1){
+            return 1;
+        }
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        dp[0][0] = 1;
+        return no_paths(dp, m-1, n-1); 
+    }
+};
