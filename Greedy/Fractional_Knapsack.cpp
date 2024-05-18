92d5fb4e3855d59e89eb20e9eb4fa05491bf97f7
Aditya Mehta <aditya.mehta@iitgn.ac.in>
2024-05-18 11:33:33 +0530

    Create Fractional_Knapsack.cpp

---
+++ Greedy/Fractional_Knapsack.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+class Solution {
+  public:
+    // Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        // MOST IMP STEP
+        sort(arr , arr+ n ,[](const auto &a , const auto &b){
+            double x = static_cast<double>(a.value) / a.weight;
+            double y = static_cast<double>(b.value) / b.weight;
+            return  x > y;
+        });
+        
+        double ans = 0.0; int i = 0;
+        while(W > 0 && i < n){
+            if(arr[i].weight <= W){
+                W -= arr[i].weight;
+                ans+= arr[i].value;
+            }
+            else{
+                ans += (static_cast<double>(arr[i].value)/arr[i].weight)*W;
+                W= 0;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
