aaa3f4a3e69b48251be1dc263815f8dbb518e4bf
aditya-me13 <aditya.mehta@iitgn.ac.in>
2024-07-08 14:51:20 +0530

    July 8

---
+++ LeetCode Daily Problems/July 2024/24-07-08.cpp
@@ -0,0 +1,13 @@
+// Problem Link: https://leetcode.com/problems/find-the-winner-of-the-circular-game/description/?envType=daily-question&envId=2024-07-08
+
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int winner = 0;  // Josephus problem is 0-indexed in calculation
+        for (int i = 1; i <= n; ++i) {
+            winner = (winner + k) % i;
+        }
+        return winner + 1;  // Convert back to 1-indexed
+    }
+};
