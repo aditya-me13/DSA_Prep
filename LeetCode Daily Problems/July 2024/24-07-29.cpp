35555c3d6bb118a31b8559086a6feee76e6036ae
aditya-me13 <aditya.mehta@iitgn.ac.in>
2024-07-29 19:01:45 +0530

    July 29

---
+++ LeetCode Daily Problems/July 2024/24-07-29.cpp
@@ -0,0 +1,22 @@
+// Problem Link : https://leetcode.com/problems/count-number-of-teams/description/?envType=daily-question&envId=2024-07-29
+
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int ans=0;
+        int n = rating.size();
+        for(int i=0; i<n-2; i++){
+            for(int j=i+1; j<n-1; j++){
+                for(int k=j+1; k<n; k++){
+                    if(rating[i] < rating[j] && rating[j] < rating[k]){
+                        ans++;
+                    }
+                    else if(rating[i] > rating[j] && rating[j] > rating[k]){
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
